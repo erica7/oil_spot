136cfe185b1aedc7e0171779d32df88c5b817faa
Erica <ericaesnider@gmail.com>
2018-04-02 16:20:13 -0500

    add test

--- OilSpot/__tests__/FormulaView.js
+++ OilSpot/__tests__/FormulaView.js
@@ -1,11 +1,24 @@
 import 'react-native';
 import React from 'react';
 import FormulaView from '../FormulaView';
+import { StackNavigator } from 'react-navigation';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const globals = require('./../Globals.js');
+const Item = globals.Item;
+// const navigation = { navigate: jest.fn() };
+
+//FIXME passing react-navigation navigate props 
+const { navigate } = this.props.navigation;
+navigate.navigation.state.params = {p: p, navTitle: "TEST"};
+
 it('renders FormulaView correctly', () => {
-  const tree = renderer.create(<FormulaView />).toJSON();
+  const tree = renderer.create(
+    <FormulaView props={navigation}  />
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+//<FormulaView navigation={navigation.state.params} p={flowrate} navTitle="test" navigation={navigation} />
